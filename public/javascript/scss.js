04c9be9e14a0c96ff8e932e65d6e2a844d55538a
Chris Cuomo <weatherfields@protonmail.com>
2020-01-17 22:10:19 -0500

    test

--- public/javascript/scss.js
+++ public/javascript/scss.js
@@ -29,3 +29,9 @@ function toggleBtn() {
     }
 
 }
+
+function getInfo() {
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+    console.log(username, password);
+}
